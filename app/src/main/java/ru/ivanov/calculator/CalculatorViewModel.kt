c27487313197f7595ab26c3884983f7d3b8dad94
KolyaIvanov1st <2kolya2012@mail.ru>
2026-03-02 12:12:09 +0300

    1stPrj

---
+++ app/src/main/java/ru/ivanov/calculator/CalculatorViewModel.kt
@@ -0,0 +1,123 @@
+package ru.ivanov.calculator
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import org.mariuszgromada.math.mxparser.Expression
+import kotlin.random.Random
+
+class CalculatorViewModel : ViewModel() {
+
+    private val _state: MutableStateFlow<CalculatorState> = MutableStateFlow(
+        CalculatorState.Initial
+    )
+    val state = _state.asStateFlow()
+
+    private var expression: String = ""
+
+    fun processComand(command: CalculatorCommand) {
+        Log.d("CalculatorViewModel", "Comand: $command")
+        when (command) {
+            CalculatorCommand.Clear -> {
+                expression = ""
+                _state.value = CalculatorState.Initial
+            }
+
+            CalculatorCommand.Evaluate -> {
+                val result = evaluate()
+                _state.value = if (result != null){
+                    CalculatorState.Seccess(result = result)
+                }else{
+                    CalculatorState.Error(expression = expression)
+                }
+            }
+
+            is CalculatorCommand.Input -> {
+                val symbol = if (command.symbol != Symbol.PARENTHESIS) {
+                    command.symbol.value
+                } else {
+                    getCorrectParenthesis()
+                }
+                expression += symbol
+                _state.value = CalculatorState.input(
+                    expression = expression,
+                    evaluate()?:""
+                )
+            }
+
+        }
+    }
+
+    private fun evaluate(): String? {
+         return expression.replace('x','*')
+             .replace(',','.')
+            .let { Expression(it) }
+            .calculate()
+            .takeIf { it.isFinite()}?.toString()
+    }
+
+
+    private fun getCorrectParenthesis(): String {
+        val openCount: Int = expression.count { it == '(' }
+        val closeCount: Int = expression.count { it == ')' }
+
+        return when {
+            expression.isEmpty() -> "("
+            expression.last().let { !it.isDigit() && it != ')' && it != 'π' } -> "("
+            openCount > closeCount -> ")"
+            else -> "("
+        }
+    }
+}
+
+sealed interface CalculatorState {
+
+    data object Initial : CalculatorState
+
+    data class input(
+        val expression: String,
+        val result: String
+    ) : CalculatorState
+
+    data class Seccess(val result: String) : CalculatorState
+
+    data class Error(val expression: String) : CalculatorState
+
+}
+
+sealed interface CalculatorCommand {
+
+    data object Clear : CalculatorCommand
+    data object Evaluate : CalculatorCommand
+    data class Input(val symbol: Symbol) : CalculatorCommand
+}
+
+enum class Symbol(val value: String) {
+    DIGIT_0("0"),
+    DIGIT_1("1"),
+    DIGIT_2("2"),
+    DIGIT_3("3"),
+    DIGIT_4("4"),
+    DIGIT_5("5"),
+    DIGIT_6("6"),
+    DIGIT_7("7"),
+    DIGIT_8("8"),
+    DIGIT_9("9"),
+    ADD("+"),
+    SUBSTRACT("-"),
+    MULTIPLY("x"),
+    DIVIDE("/"),
+    PERCENT("%"),
+    POWER("^"),
+    FACTORIAL("!"),
+    SQRT("√"),
+    PI("π"),
+    DOT(","),
+    PARENTHESIS("()")
+}
+
+data class Display(
+    val expression: String,
+    val result: String,
+)
